7e2ad4f05775c648244ac970ce649ba00e7187c5
Dan Leinir Turthra Jensen <admin@leinir.dk>
2022-05-23 15:46:43 +0100

    PlayGridManager: Match octaves for the midi note names with elsewhere

--- src/PlayGridManager.cpp
+++ src/PlayGridManager.cpp
@@ -55,7 +55,6 @@ static const QStringList sceneNames{"A", "B", "C", "D", "E", "F", "G", "H", "I",
 
 static const QStringList midiNoteNames{
     "C-1", "C#-1", "D-1", "D#-1", "E-1", "F-1", "F#-1", "G-1", "G#-1", "A-1", "A#-1", "B-1",
-    "C-1", "C#-1", "D-1", "D#-1", "E-1", "F-1", "F#-1", "G-1", "G#-1", "A-1", "A#-1", "B-1",
     "C0", "C#0", "D0", "D#0", "E0", "F0", "F#0", "G0", "G#0", "A0", "A#0", "B0",
     "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
     "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
@@ -64,7 +63,8 @@ static const QStringList midiNoteNames{
     "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
     "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6",
     "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7",
-    "C8", "C#8", "D8", "D#8", "E8", "F8", "F#8", "G8"
+    "C8", "C#8", "D8", "D#8", "E8", "F8", "F#8", "G8", "G#8", "A8", "A#8", "B8",
+    "C9", "C#9", "D9", "D#9", "E9", "F9", "F#9", "G9"
 };
 
 Q_GLOBAL_STATIC(QList<PlayGridManager*>, timer_callback_tickers)
